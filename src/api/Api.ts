af43be44457691597243bc3c47a441ce9f6f327f
dherv <damien.herv@gmail.com>
2020-02-01 12:02:33 +0900

    feat(api): add api post request

--- src/api/Api.ts
+++ src/api/Api.ts
@@ -13,7 +13,29 @@ export default class Api {
       })
       .catch(error => console.log(error));
   }
-  static post() {}
+  static post(endpoint: string, body: object) {
+    return fetch(this.url(endpoint), {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      mode: "cors", // no-cors, *cors, same-origin
+      cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+      credentials: "same-origin", // include, *same-origin, omit
+      headers: {
+        "Content-Type": "application/json"
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      redirect: "follow", // manual, *follow, error
+      referrerPolicy: "no-referrer", // no-referrer, *client
+      body: JSON.stringify(body) // body data type must match "Content-Type" header
+    })
+      .then(response => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error();
+        }
+      })
+      .catch(error => console.log(error));
+  }
   static put() {}
   static delete() {}
 }
