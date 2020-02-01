28344c15f2f8e86daf08ca0b32ce1faf7c3586ce
dherv <damien.herv@gmail.com>
2020-02-01 10:46:38 +0900

    feat(grammars): get grammars from api

--- src/pages/Grammars.tsx
+++ src/pages/Grammars.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Layout from "../components/layout/Layout";
 import {
   AsideLeft,
@@ -7,40 +7,31 @@ import {
   AsideRecentDailies
 } from "../components/layout/Asides";
 import { Main, MainTitle } from "../styled/GlobalComponents";
-import { IWord } from "../types/interfaces";
+import { IGrammar } from "../types/interfaces";
 import MainListItemWithPanel from "../components/layout/MainListItemWithPanel";
 import DotWithWord from "../components/ui/DotWithWord";
+import Api from "../api/Api";
+import Utils from "../utils/Utils";
 
 const Grammars = () => {
-  const sampleWords: Array<IWord> = [
-    {
-      text: "grammar1",
-      translation: "grammar1",
-      type: "grammars",
-      createdAt: "2020/01/01",
-      example: "Word 1 example sentence on click",
-      timesUsed: 3
-    },
-    {
-      text: "grammar2",
-      translation: "grammar2",
-      type: "grammars",
-      createdAt: "2020/01/01",
-      example: "Word 2 example sentence on click",
-      timesUsed: 3
-    }
-  ];
+  const [grammars, setGrammars] = useState<IGrammar[]>([]);
+
+  useEffect(() => {
+    Api.get("/grammars").then((data: IGrammar[]) => {
+      setGrammars(data);
+    });
+  }, []);
 
   const displayAsideLeft = () => <AsideLeftDefault />;
   const displayAsideRight = () => <AsideRecentDailies />;
 
-  const displayListItemPanel = (itemDetails: IWord) => {
+  const displayListItemPanel = (itemDetails: IGrammar) => {
     return (
       <>
         <p>{itemDetails.example}</p>
         <p>
-          <span>Added on the {itemDetails.createdAt}</span>{" "}
-          <span>used {itemDetails.timesUsed} times</span>
+          <span>Added on the {Utils.DateFormat(itemDetails.createdAt)}</span>
+          {/* <span>used {itemDetails.timesUsed} times</span> */}
         </p>
       </>
     );
@@ -51,20 +42,20 @@ const Grammars = () => {
       <Main>
         <MainTitle>Grammars</MainTitle>
         <ul>
-          {sampleWords.map((w, i) => (
+          {grammars.map((g, i) => (
             <MainListItemWithPanel
-              key={`${i}_${w.text}`}
+              key={`${i}_${g.text}`}
               itemIndex={i}
-              additionalText={w.translation}
-              itemDetails={w}
+              additionalText={g.translation}
+              itemDetails={g}
               listItemContent={
                 <DotWithWord
-                  typeOrColor={w.type}
-                  word={w.text}
-                  translation={w.translation}
+                  typeOrColor="grammars"
+                  word={g.text}
+                  translation={g.translation}
                 />
               }
-              listItemPanelContent={displayListItemPanel(w)}
+              listItemPanelContent={displayListItemPanel(g)}
             ></MainListItemWithPanel>
           ))}
         </ul>
